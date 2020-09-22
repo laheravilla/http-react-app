eaedadff7d82f92f5c837cb4f9c8e8105eb944b7
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-09-22 07:06:52 +0200

    fetched data on update without creating infinite loops

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -3,13 +3,32 @@ import React, { Component } from 'react';
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        post: null
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.id) {
+            // To avoid the infinite loop
+            if (!this.state.post || (this.state.post && this.state.post.id !== this.props.id)) {
+                fetch(`http://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+                    .then(response => response.json())
+                    .then(post => this.setState({post}))
+                    .catch(e => console.log(e));
+            }
+        }
+    }
+
     render () {
         let post = <p style={{textAlign: "center"}}>Please select a Post!</p>;
         if (this.props.id) {
+            post = <p>Loading...</p>
+        }
+        if (this.state.post) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.post.title}</h1>
+                    <p>{this.state.post.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
