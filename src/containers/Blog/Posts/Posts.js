082da27e30bbaf59a1a70f1fd22a99d3435feddb
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-09-29 06:57:36 +0200

    navigating programmatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -7,6 +7,7 @@ class Posts extends Component {
     state = { posts: [] }
 
     postSelectedHandler = (id) => this.setState({selectedPostId: id});
+    // postSelectedHandler = (id) => this.props.history.push({pathname: "/" + id}); // Navigate programmatically, once request is done
 
     async componentDidMount() {
         try {
@@ -29,6 +30,14 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
+                    /*
+                        <Post
+                            key={post.id}
+                            title={post.title}
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)}
+                        />
+                     */
                     <Link to={"/" + post.id} key={post.id}>
                         <Post
                             title={post.title}
